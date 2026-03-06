a3c98e57d676699e6af7eeab3a95e0e22c2f0e78
Xojiakbar Tojiboyev <xojiakbar519@gmail.com>
2026-03-06 14:41:23 +0500

    Create 3-dars topshiriqlar.py

---
+++ 3-dars topshiriqlar.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  6 13:52:48 2026
+
+@author: HP
+"""
+# 3-dars
+print('5 ning 4-darajasi', 5**4)
+print('22 ni 4 ga bo\'lganda qancha qoldiq qoladi?', 22%4)
+print('Tomonlari 125 ga teng kvadratning yuzi :', 125**2, 
+      '\n Tomonlari 125 ga teng kvadratning perimetri :', 125*4)
+print('Diametri 12 ga teng bo\'lgan doiraning yuzini toping (π=3.14deb oling)',
+      3.14*(12/2)**2)
+print('Katetlari 6 va 7 bo\'lgan to\'g\'ri burchakli uchburchakning gipotenuzasini toping',
+      (6**2+7**2)**(1/2))
