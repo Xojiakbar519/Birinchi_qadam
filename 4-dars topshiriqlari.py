1a6daaf255b88aba2b6e15ded963ec451abe08fd
Xojiakbar Tojiboyev <xojiakbar519@gmail.com>
2026-03-06 14:41:26 +0500

    Create 4-dars topshiriqlari.py

---
+++ 4-dars topshiriqlari.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  6 13:52:51 2026
+
+@author: HP
+"""
+# 4-dars
+# "Hello World!"matnini yangi ozgaruvchiga yuklang va print()konsolga chiqaring
+soz='Hello world'
+print(soz)
+# xabardeb nomlangan o'zgaruvchiga biror matn yuklang va konsolga chiqaring, 
+# keyin esa o'zgaruvchiga yangi qiymat berib uni ham konsolga chiqaring.
+ozgaruvchi='Assalomu alaykum'
+print(ozgaruvchi)
+# classden nomi o'zgaruvchi yarating, unga biror qiymat bering va konsolga chiqaring
+"javob: O'zgaruvchini class deb nomlash mumkin emas, sababi class bu maxsus kalit so'z "
+# Quyidagi kodni bajaring:
+radius = 5
+pi = 3.14159
+aylana_yuzi = pi * radius**2
+print("Radiusi" , radius, "ga teng aylananing yuzi=", aylana_yuzi)
