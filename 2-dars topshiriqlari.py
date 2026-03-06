cf3e5853bc14a2ad7eb82558f5efdc1e1997ab9a
Xojiakbar Tojiboyev <xojiakbar519@gmail.com>
2026-03-06 14:41:18 +0500

    Create 2-dars topshiriqlari.py

---
+++ 2-dars topshiriqlari.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  6 13:49:33 2026
+
+@author: HP
+"""
+# 2- dars topshiriqlari:
+print('Assalom alaykum')
+print('Hayrli tong!')
+print(2+4*2)
+print(19/5)
+print(2**4)
