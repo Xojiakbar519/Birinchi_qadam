78c68ff0c08ddce6e39761788a88cefabe3085ca
Xojiakbar Tojiboyev <xojiakbar519@gmail.com>
2026-03-17 22:07:38 +0500

    Create 14-dars topshiriqlari.py

---
+++ 14-dars topshiriqlari.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 17 09:52:41 2026
+
+@author: HP
+"""
+# 14-dars
+# # get metodidan foydalanish
+# yashashjoyi={
+#     'Soliha':'Uzbekiston',
+#     'Yuliya':'Russian',
+#     'Erce':'Turtkiya'
+#     }
+# a=input('kim: ')
+# manzil=yashashjoyi.get(a, 'Malumot yoq')
+# print(manzil)
+
+## #otam (onam, akam, ukam, va hokazo) degan lug'at yarating va lug'atga shu inson haqida kamida 3 ta m'alumot kiriting (ismi, tu'gilgan yili, shahri, manzili va hokazo). Lug'atdagi ma'lumotni matn shaklida konsolga chiqaring :Otamning ismi Mavlutdin, 1954-yilda, Samarqand viloyatida tug'ilgan
+
+# malumotlar={
+# 'otam':{
+#       'ismi':'Valijon',
+#       'familyasi':'Tursunov',
+#       'yoshi':'50'
+#       },
+# 'akam':{
+#       'ismi':'Nurbek',
+#       'familyasi':'Valiyev',
+#       'yoshi':'23'
+#       },
+# 'opam':{
+#       'ismi':'Sinura',
+#       'familyasi':'Valiyeva',
+#       'yoshi':'20'
+#       }
+# }
+# a=str(input("kim: "))
+# b=str(input("qanday malumot: "))
+# # print(malumotlar[a].get(b, 'Malumot yoq'))
+# if malumotlar.get(a):
+#     print(malumotlar[a].get(b, 'Malumot yoq'))
+# else: 
+#     print('Malumot yoq')
+
+## Oila a'zolaringizning sevimli taomlari lug'atini tuzing. Lug'atda kamida 5 ta ism-taom jufltigi bo'lsin. Kamida uch kishining sevimli taomini konsolga chiqaring: Alining sevimli taomi osh
+
+# sevimli={
+#     'Alijon':'mastava',
+#     'Valijon':'sho\'rva',
+#     'Sabina':'shashlik',
+#     'Gulasal':'chuchvara'
+#     }
+# a=str(input('oladagi kimni sevimli taomini bilmoqchisiz ? \n:'))
+# print(f'uning sevimli taomi: {sevimli.get(a,'Bunday oila azosi yoq!')}')
+
+
