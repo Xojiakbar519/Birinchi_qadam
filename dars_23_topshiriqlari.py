ea94aa989f3fe1352b6f25d866fc00326c89e0fa
Xojiakbar Tojiboyev <xojiakbar519@gmail.com>
2026-04-04 13:39:19 +0500

    Create dars_23_topshiriqlari.py

---
+++ dars_23_topshiriqlari.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Apr  4 12:12:48 2026
+
+@author: HP
+"""
+# 23-dars 
+# Modullar bilan ishlash 
+from dars_22_topshiriqlari import kopaytma
+
+kopaytma(22,10,33)
+
+# dars_22_topshiriqlari.malumotnoma('Xusnora','turdibekova')
