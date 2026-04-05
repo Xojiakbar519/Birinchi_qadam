137fc2ee6c5a72eccae9c64b6dfcf1e7c12b2b38
Xojiakbar Tojiboyev <xojiakbar519@gmail.com>
2026-04-05 12:15:42 +0500

    Create 1 dars.py

---
+++ 1 dars.py
@@ -0,0 +1,43 @@
+#a=str(input('ism kiriting '))
+#b=int(input('yoshingiz nechida '))
+#print('salom',a, 'siz',b,'yoshdasiz' )
+
+
+#a=float(input('1-son:'))
+#b=float(input('2-son:'))
+#print('sonlar yigindisi = ', a+b)
+
+
+#a=int(input('yoshingiz nechida '))
+#if a>=18:
+#    print('Ruxsat')
+#else:
+#    print('yoshingiz kichik')
+
+
+#a=int(input('son kiriting: '))
+#if a>0:
+#    print('musbat son')
+#elif a==0:
+#        print('0')
+#else:
+#    a<0
+#    print('manfiy son')
+
+
+#print('Assalomu alaykum')
+#a=str(input('parolni kiriting: '))
+#if a=="python123":
+#    print('Xush kelibsiz')
+#else:
+#    print('xato !')
+
+a=int(input('baho kiriting: '))
+if a==5:
+    print("Alo")
+elif a==4:
+    print('yaxshi')
+elif a==3:
+    print('qoniqarli')
+else:
+    print('qoniqarsiz')
