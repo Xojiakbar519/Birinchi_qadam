80bdc26928d14e665d73339293e2e6bb8ff5b63f
Xojiakbar Tojiboyev <xojiakbar519@gmail.com>
2026-04-05 12:15:46 +0500

    Create 2-dars.py

---
+++ 2-dars.py
@@ -0,0 +1,34 @@
+#mevalar=['olma','banan','gilos']
+#for meva in mevalar:
+#    print(meva)
+
+#for i in range(1,11):
+#    print(i)
+
+#xol=[3,5,7,2,3]
+#total=0
+#for n in xol:
+#    total+=n
+#print(total)
+
+#shun='python'
+#for harf in shun:
+#    print(harf)
+
+#i=1
+#while i<=5:
+#    print(i)
+#    i+=1
+
+#son=int(input('son= '))
+#while son !=78:
+#    son=int(input('yana='))
+
+
+nums=[1,2,3,4,5,6,7,8,9]
+juftlar=[]
+for n in nums:
+    if n%2==0:
+        juftlar.append(n)
+print(juftlar)
+    
