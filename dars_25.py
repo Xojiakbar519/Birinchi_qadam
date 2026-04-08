9ae82ca1e92f58f065067f68e997b5b4afb88845
Xojiakbar Tojiboyev <xojiakbar519@gmail.com>
2026-04-08 22:49:44 +0500

    Update dars_25.py

--- dars_25.py
+++ dars_25.py
@@ -25,27 +25,64 @@ import random
 #             print('harakat qilib ko\'ring')
         
 #     break
-print('Bir son oylang ')
-b=int(input('necha xonali ?: '))
-hajmi=10**b
-a=0
+
+
+
+# print('0 dan 100 gacha son oylang ')
+# royhat=[]
+# a=0
+# b=100
+# while True:
+#     tasodif=random.randint(a, b)
+#     if len(royhat)>90:
+#         print('ERROR. Hamma son aytildi !!! ')
+#         break
+#     elif tasodif in royhat:
+#         continue
+#     elif tasodif < 10 :
+#         continue
+#     else:
+#         print(f'{a} va {b} lar orasidan:')
+#         royhat.append(tasodif)
+#         print(' siz o\'ylagan son : ' , tasodif )
+#         soroq=input(' to\'g\'rimi ? ha/yoq')
+#         if soroq=='ha':
+#             print(' son topildi ! ')
+#             break
+#         savol=(input('ayta olasizmi son kattaroqmi yoki kichikroq? katta / kichik')).lower()
+#         if savol=='katta':
+#             a=tasodif
+#         elif savol=='kichik':
+#             b=tasodif
+#         if a+1==b:
+#             print(' Hatolik orada son qolmadi tanlashga !')
+#             break
+
+a = 0
+b = 1000
+
 while True:
-    k_oy=random.randint(a,hajmi)
-    print(f' {k_oy} tog\'rimi? ')
-    javob=input('to\'ri bo\'lsa h, Xato bo\'lsa y: ')
-    if javob=='h':
-        print('Topdimmi 😁')
+    tasodif = (a + b) // 2
+    print(f"{a} va {b} orasidan taxmin: {tasodif}")
+
+    soroq = input("to‘g‘rimi? (ha/yoq): ").lower()
+
+    if soroq == 'ha':
+        print('Son topildi!')
         break
-    else: 
-        savol=input('kattaroqmi kichikromi?  katta bo\'lsa k yoki ki: ')
-        if savol=='k':
-            a = k_oy+1
+
+    savol = input("kattaroqmi yoki kichikroq? (katta/kichik): ").lower()
+
+    if savol == 'katta':
+        a = tasodif + 1
+    elif savol == 'kichik':
+        b = tasodif - 1
+
+    if a > b:
+        print("Xatolik! Noto‘g‘ri javob berildi.")
+        break
             
-            
-        elif savol=='ki': 
-            hajmi = k_oy-1
-            
-        
+150      
          
       
         
